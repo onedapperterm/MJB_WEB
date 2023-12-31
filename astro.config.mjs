a917b647859d30935e1a38bd12e9ad7ed2de6b37
May <mayraort95@icloud.com>
2023-12-31 14:53:16 +0100

    feat: Countdown ui and responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/onedapperterm',
-  base: '/MJB_WEB.git'
+  //site: 'https://github.com/onedapperterm',
+  //base: '/MJB_WEB.git'
 });
