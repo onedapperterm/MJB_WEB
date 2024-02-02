d9074ebd0227f958ed646b3b02dca560cda7525b
Julian <julian.castro@binaroo.com>
2024-02-02 18:20:45 +0100

    feat: Authentication preview.
    Guard an athentication form added.
    index content moved to new home and instead placed login form.

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -13,22 +13,26 @@ export const POST: APIRoute = async ({ request }) => {
 
   try {
 
+    const guestLogin = {
+      firstName: body.firstName,
+      lastName: body.lastName,
+      password: body.password
+    }
+
     const guestsSnapshot = await firestore.collection('guests').get();
     const guestList: Guest[] = guestsSnapshot.docs.map(doc => doc.data() as Guest);
 
-    const guest: Guest | undefined = guestList.find(guest => guest.familyName === body.guestName);
-    if (!guest) {
-      return new Response( JSON.stringify({error: 'wrong password or user'}), { status: 400, headers: { location: "/" } });
+    const guest: Guest | undefined = guestList.find(guest => guest.familyName.includes(guestLogin.firstName));
+    if (!guest || guestLogin.password != "12341234") {
+      return new Response( JSON.stringify({error: 'wrong password or user'}), { status: 400 });
     } else {
       // logic to create the cookie or session
       return new Response(JSON.stringify(guest), {status: 200});
     }
 
-
   } catch (error) {
 
     console.error(error)
-
     return new Response("Something went wrong", {
       status: 500,
     });
